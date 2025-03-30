b34918d8685d80cd1cbb2d98797e2afd65f6b61b
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-30 19:52:21 -0400

    Update

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,9 +8,16 @@ function Login(){
         <>
         <div>
 
+        <h1>Product List</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>{product.title} - ${product.price}</li>
+        ))}
+      </ul>
 
 
-          
+
+
         </div>
         
         </>
