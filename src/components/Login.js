2a8d6fc9d6dbd04a0f2d099a6be64a84ef676fdb
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-30 19:55:57 -0400

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,13 @@
-function Login(){
-
+function Login() {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleSubmit = (event) =>{
+  const handleSubmit = (event) => {
     event.preventDefault();
     const storedUsers = JSON.parse(localStorage.getItem('myuser')) || [];
 
     const validUser = storedUsers.find(user => user.userName === name && user.userPassword === password);
-
 
     if (validUser) {
       alert(`Login successful! Welcome ${validUser.userRole}`);
@@ -23,30 +21,13 @@ function Login(){
     }
   };
 
-
-
-
-
-  }
-
-    return(
-        <>
-    
-       
-        <form onSubmit={handleSubmit}>
-
+  return (
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
-
       <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
       <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
       <button type="submit">Login</button>
       {error && <p style={{ color: 'red' }}>{error}</p>}
     </form>
-
-
-        
-        </>
-    );
+  );
 }
-
-export default Login;
