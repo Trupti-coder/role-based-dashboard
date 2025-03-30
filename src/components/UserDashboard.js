02c79353b26a82a8e631cae3810afe91c08ca57b
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-30 19:56:26 -0400

    Update

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,6 +1,13 @@
 function UserDashboard(){
     const [products, setProducts] = useState([]);
 
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+          .then(res => res.json())
+          .then(data => setProducts(data));
+      }, []);
+
     return(
         <>
 
