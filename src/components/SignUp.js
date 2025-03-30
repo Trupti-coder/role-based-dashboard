e2824093c2992f87a07003fb19ba6711d59480a1
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-29 21:09:31 -0400

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 function SignUp(){
+
+    const [name, setName] = useState('');
+  const [city, setCity] = useState('');
+  const [dob, setDob] = useState('');
     return(
 
         <>
