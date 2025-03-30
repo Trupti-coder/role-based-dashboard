d0d02037cf9af146a9288358fb24f9a63e23f92b
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-29 21:10:08 -0400

    Update

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,6 +5,10 @@ function SignUp(){
     const [name, setName] = useState('');
   const [city, setCity] = useState('');
   const [dob, setDob] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
+
     return(
 
         <>
