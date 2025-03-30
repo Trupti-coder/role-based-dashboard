666ac73f70862727c1bb69793aa36f2bb3bbc3a3
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-30 19:53:54 -0400

    Update

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,13 @@ function Login(){
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+  const handleSubmit = (event) =>{
+
+
+
+    
+  }
+
     return(
         <>
     
@@ -11,7 +18,7 @@ function Login(){
         <form onSubmit={handleSubmit}>
 
       <h2>Login</h2>
-      
+
       <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
       <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
       <button type="submit">Login</button>
