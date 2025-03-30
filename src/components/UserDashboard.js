bffbe2937d9fc24e1500e6ace2147fa38240fe56
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-30 19:56:53 -0400

    Update

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -11,6 +11,15 @@ function UserDashboard(){
     return(
         <>
 
+<div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>{product.title} - ${product.price}</li>
+        ))}
+      </ul>
+    </div>
+
 
         </>
     );
