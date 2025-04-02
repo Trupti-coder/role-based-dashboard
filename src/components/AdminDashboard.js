c64d612a90d048523899653bbb539b52fa0a8032
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-01 20:01:10 -0400

    Update AdminDashboard.js

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react";
+
 function AdminDashboard(){
+
+    const [products,setProducts]=useState([]);
+
+    useEffect(()=>{
+        
+
+    })
     return(
         <>
         
