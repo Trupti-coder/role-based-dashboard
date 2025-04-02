76b761cf26efaaa59533735288decd2d2e178237
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-01 20:04:36 -0400

    Update

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -6,8 +6,16 @@ function AdminDashboard(){
 
     useEffect(()=>{
 
+        fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    }, [])
 
-    })
+
+
+
+
+    
     return(
         <>
         <div>
