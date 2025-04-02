397c2e694986d489c899b0dbaa5b0104ac1025e5
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-01 21:56:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 function App() {
   return (
     <>
 
 <Routes>
-        <Route path="/signup"  element={} />
-        <Route path="/login"  />
+        <Route path="/signup"  element={<SignUp />} />
+        <Route path="/login"  element={<Login />} />
         <Route path="/user"  />
         <Route path="/admin"  />
 
