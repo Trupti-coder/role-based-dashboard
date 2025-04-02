a25025264f73ab5762cfbf20a8ac5a608412b1d4
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-04-01 20:03:05 -0400

    Update

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -5,11 +5,20 @@ function AdminDashboard(){
     const [products,setProducts]=useState([]);
 
     useEffect(()=>{
-        
+
 
     })
     return(
         <>
+        <div>
+            <h1>Admin Panel - Manage products</h1>
+            <button onClick={addProduct}>Add Product</button>
+
+            <ul>
+
+              
+            </ul>
+        </div>
         
         
         </>
